6bc35625c0fa02c54824d1bc9712225d458c9715
warrenniu <warrenniu@gmail.com>
2020-10-30 19:06:36 -0400

    Commit

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,15 +2,33 @@ class PostsController < ApplicationController
   before_action :set_post!, only: [:show, :edit, :update]
 
   def show
+    @post = Post.find(params[:id])
   end
+
+  # def new
+  #   @post = Post.new
+  # end
 
   def edit
+    @post = Post.find(params[:id])
   end
 
-  def update
-    @post.update(post_params)
+  # def create
+  #   @post = Post.new(post_params)
+  #   if @post.valid?
+  #     @post.save
+  #     redirect_to post_path(@post)
+  #   else
+  #     render :new
+  # end
 
-    redirect_to post_path(@post)
+  def update
+    @post = Post.find(params[:id])
+    if @post.update(post_params)
+        redirect_to post_path(@post)
+    else
+      render :edit
+    end
   end
 
   private
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,8 @@
 class Post < ActiveRecord::Base
+    validates :title, presence: true
+    validates :category, inclusion: { in: ["Fiction", "Non-Fiction"]}
+    validates :content, length: { minimum: 100 }
 end
+
+
+
